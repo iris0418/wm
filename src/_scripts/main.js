ab45ea1eedc7d47c3241affefd1d0a57d56e3e6f
Iris Zhou <iris.zhou@adelphi.digital>
2018-08-31 17:33:03 +1000

    working on slide

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,6 +6,19 @@
 import $ from 'jquery';
 
 $(() => {
-  // new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+  // setInterval(function(){
+  //   var $this = $('.section_wmfans .wmfan:last-of-type');
+  //   var $next = $this.parent().children().first();
+  //   $next.clone().appendTo($this.parent()).fadeIn();
+  //   $('.section_wmfans').find(' .wmfan').css('animation','slide 2s linear infinite');
+  //   $this.siblings('.wmfan:first').remove();
+  // },2000);
+    
+  (function(){ 
+    var $this = $('.section_wmfans .wmfan:last-of-type');
+    var $next = $this.parent().children().first();
+    $next.clone().appendTo($this.parent()).fadeIn();
+    $('.section_wmfans').find(' .wmfan');
+    $this.siblings('.wmfan:first').remove();
+  })();
 });
